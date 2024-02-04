f47205c1834770ed1754c4e05b58950d04bef1f6
Yana160916 <144559656+Yana160916@users.noreply.github.com>
2024-02-04 13:23:54 +0200

    update

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,21 +1,20 @@
-const feedbackForm = document.querySelector('.feedback-form');
-
-feedbackForm.addEventListener('input', (event) => {
-  if (event.target.tagName === 'INPUT' || event.target.tagName === 'TEXTAREA') {
-   
-    const emailValue = feedbackForm.elements.email.value;
-    const messageValue = feedbackForm.elements.message.value;
-
-    const feedbackState = {
-      email: emailValue,
-      message: messageValue,
-    };
-
-    localStorage.setItem('feedback-form-state', JSON.stringify(feedbackState));
-  }
-});
-
 document.addEventListener('DOMContentLoaded', () => {
+  const feedbackForm = document.querySelector('.feedback-form');
+
+  feedbackForm.addEventListener('input', (event) => {
+    if (event.target.tagName === 'INPUT' || event.target.tagName === 'TEXTAREA') {
+      const emailValue = feedbackForm.elements.email.value;
+      const messageValue = feedbackForm.elements.message.value;
+
+      const feedbackState = {
+        email: emailValue,
+        message: messageValue,
+      };
+
+      localStorage.setItem('feedback-form-state', JSON.stringify(feedbackState));
+    }
+  });
+
   const savedState = localStorage.getItem('feedback-form-state');
 
   if (savedState) {
@@ -24,16 +23,16 @@ document.addEventListener('DOMContentLoaded', () => {
     feedbackForm.elements.email.value = parsedState.email || '';
     feedbackForm.elements.message.value = parsedState.message || '';
   }
-});
 
-feedbackForm.addEventListener('submit', (event) => {
-  event.preventDefault();
+  feedbackForm.addEventListener('submit', (event) => {
+    event.preventDefault();
 
-  localStorage.removeItem('feedback-form-state');
-  feedbackForm.reset();
+    localStorage.removeItem('feedback-form-state');
+    feedbackForm.reset();
 
-  console.log({
-    email: feedbackForm.elements.email.value,
-    message: feedbackForm.elements.message.value,
+    console.log({
+      email: feedbackForm.elements.email.value,
+      message: feedbackForm.elements.message.value,
+    });
   });
 });
