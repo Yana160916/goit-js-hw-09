1ef9d41dd605811aa43736beb2b98e904931dd57
Yana160916 <144559656+Yana160916@users.noreply.github.com>
2024-02-04 13:40:36 +0200

    update

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,38 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
   const feedbackForm = document.querySelector('.feedback-form');
 
-  feedbackForm.addEventListener('input', (event) => {
-    if (event.target.tagName === 'INPUT' || event.target.tagName === 'TEXTAREA') {
-      const emailValue = feedbackForm.elements.email.value;
-      const messageValue = feedbackForm.elements.message.value;
+  if (feedbackForm) { // Проверяем, что элемент найден
+    feedbackForm.addEventListener('input', (event) => {
+      if (event.target.tagName === 'INPUT' || event.target.tagName === 'TEXTAREA') {
+        const emailValue = feedbackForm.elements.email.value;
+        const messageValue = feedbackForm.elements.message.value;
 
-      const feedbackState = {
-        email: emailValue,
-        message: messageValue,
-      };
+        const feedbackState = {
+          email: emailValue,
+          message: messageValue,
+        };
 
-      localStorage.setItem('feedback-form-state', JSON.stringify(feedbackState));
-    }
-  });
-
-  const savedState = localStorage.getItem('feedback-form-state');
-
-  if (savedState) {
-    const parsedState = JSON.parse(savedState);
-
-    feedbackForm.elements.email.value = parsedState.email || '';
-    feedbackForm.elements.message.value = parsedState.message || '';
-  }
-
-  feedbackForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-
-    localStorage.removeItem('feedback-form-state');
-    feedbackForm.reset();
-
-    console.log({
-      email: feedbackForm.elements.email.value,
-      message: feedbackForm.elements.message.value,
+        localStorage.setItem('feedback-form-state', JSON.stringify(feedbackState));
+      }
     });
-  });
+
+    const savedState = localStorage.getItem('feedback-form-state');
+
+    if (savedState) {
+      const parsedState = JSON.parse(savedState);
+
+      feedbackForm.elements.email.value = parsedState.email || '';
+      feedbackForm.elements.message.value = parsedState.message || '';
+    }
+
+    feedbackForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+
+      localStorage.removeItem('feedback-form-state');
+      feedbackForm.reset();
+
+      console.log({
+        email: feedbackForm.elements.email.value,
+        message: feedbackForm.elements.message.value,
+      });
+    });
+  } else {
+    console.error('Element with class "feedback-form" not found.');
+  }
 });
